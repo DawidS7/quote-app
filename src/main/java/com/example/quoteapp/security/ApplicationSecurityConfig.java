6307c0f5c7246a3fdf7d09c91ecf2f5199616857
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-07-14 14:34:18 +0200

    Add ApplicationSecurityConfig class
    Added basic auth to application

---
+++ src/main/java/com/example/quoteapp/security/ApplicationSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.quoteapp.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class ApplicationSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+
+        return http.build();
+    }
+
+
+}
