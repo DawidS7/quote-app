3f0ead65ad3ca10e03ef7d1579df901f0f7f1671
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-07-14 08:52:43 +0200

    Add Quotation class
    -Added author and content fields

---
+++ src/main/java/com/example/quoteapp/domain/Quotation.java
@@ -0,0 +1,14 @@
+package com.example.quoteapp.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Quotation {
+
+    private String author;
+    private String content;
+
+}
