1dd5791cd3e78cc4ee5205ee0b0c2d99a041ce99
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-07-15 15:33:33 +0200

    Modify ApplicationUserRole
    -Added set of permission to any role

--- src/main/java/com/example/quoteapp/security/ApplicationUserRole.java
+++ src/main/java/com/example/quoteapp/security/ApplicationUserRole.java
@@ -1,7 +1,19 @@
 package com.example.quoteapp.security;
 
+import com.google.common.collect.Sets;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+import static com.example.quoteapp.security.ApplicationUserPermission.*;
+
+
+@RequiredArgsConstructor
 public enum ApplicationUserRole {
-    USER,
-    MODERATOR,
-    ADMIN
+    USER(Sets.newHashSet(QUOTE_READ)),
+    MODERATOR(Sets.newHashSet(QUOTE_READ, QUOTE_ADD)),
+    ADMIN(Sets.newHashSet(QUOTE_READ, QUOTE_ADD, QUOTE_DELETE));
+
+    private final Set<ApplicationUserPermission> permissions;
+
 }
