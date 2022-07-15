9502e3617824e5f89039f2cb9c8fd3f3d8cdbd96
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-07-15 12:23:18 +0200

    Modify ApplicationSecurityConfig
    -Added PasswordEncoder field
    -Added @RequiredArgsConstrucotr annotation
    -Modified userDetailsService method to proper encode user passwords

--- src/main/java/com/example/quoteapp/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/quoteapp/security/ApplicationSecurityConfig.java
@@ -1,17 +1,22 @@
 package com.example.quoteapp.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-
+@RequiredArgsConstructor
 @Configuration
 public class ApplicationSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -31,19 +36,19 @@ public class ApplicationSecurityConfig {
     public UserDetailsService userDetailsService() {
         UserDetails regularUser = User.builder()
                 .username("user")
-                .password("password")
+                .password(passwordEncoder.encode("password"))
                 .roles("USER")
                 .build();
 
         UserDetails moderatorUser = User.builder()
                 .username("moderator")
-                .password("moderatorpassword")
+                .password(passwordEncoder.encode("moderatorpassword"))
                 .roles("MODERATOR")
                 .build();
 
         UserDetails adminUser = User.builder()
                 .username("admin")
-                .password("adminpassword")
+                .password(passwordEncoder.encode("adminpassword"))
                 .roles("ADMIN")
                 .build();
 
