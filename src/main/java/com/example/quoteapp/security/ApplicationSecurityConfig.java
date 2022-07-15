e576be7bcdc0417bcd4c54239467c10d4dcc9f3a
Dawid Stużyński <dawid.stuzynski@gmail.com>
2022-07-15 12:13:29 +0200

    Modify ApplicationSecurityConfig
    -Added userDetailsService method

--- src/main/java/com/example/quoteapp/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/quoteapp/security/ApplicationSecurityConfig.java
@@ -3,6 +3,10 @@ package com.example.quoteapp.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -21,6 +25,30 @@ public class ApplicationSecurityConfig {
                 .httpBasic();
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails regularUser = User.builder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        UserDetails moderatorUser = User.builder()
+                .username("moderator")
+                .password("moderatorpassword")
+                .roles("MODERATOR")
+                .build();
+
+        UserDetails adminUser = User.builder()
+                .username("admin")
+                .password("adminpassword")
+                .roles("ADMIN")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(regularUser, moderatorUser, adminUser);
     }
 
 
